bcb1cc6b041c7247a9cfaecc9d4f3a48d4548c17
Naseer Ali <naseeralisoomro44@gmail.com>
2023-02-06 12:47:35 +0500

    some changes

---
+++ src/Http/Controllers/ContactController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Basecamp\Contact\Http\Controllers;
+use Basecamp\Contact\Models\Contact;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        return view('contact::contact');
+    }
+
+    public function register()
+    {
+        return view('contact::register');
+    }
+
+    public function store(Request $request)
+    {
+        Contact::create($request->all());
+        return redirect()->back()->with('message', 'Contact Information Saved Successfully');
+    }
+}
---
+++ src/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Basecamp\Contact\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+
+Route::group(['namespace' => 'Basecamp\Contact\Http\Controllers'], function(){
+    Route::get('/contact', 'ContactController@index');
+    Route::post('/contact', 'ContactController@store');
+    Route::get('/register', 'ContactController@register');
+});
